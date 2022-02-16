11aa06f668a87025b9157a92a02b47fd210fef38
Yusuf Demirci <ydemirci7@gmail.com>
2022-02-16 13:41:21 +0300

    Updates

---
+++ precache-manifest.99190d3a35cc7c88ff23fd0c49473445.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f991ce748da449a9db5ea86ae16786",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/Keeper-App/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "2281044ed246a013905f",
+    "url": "/Keeper-App/static/js/main.4ebd41fd.chunk.js"
+  },
+  {
+    "revision": "660c3a2d9cc725da8ecd",
+    "url": "/Keeper-App/static/js/runtime-main.78711218.js"
+  }
+]);
